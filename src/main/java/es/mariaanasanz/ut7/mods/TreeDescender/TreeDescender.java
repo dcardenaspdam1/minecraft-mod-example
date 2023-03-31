8dc259c5f5191c626f72f388adfaf4abde71da5c
dcardenaspdam1 <dcardenaspdam1@gmail.com>
2023-03-31 23:59:15 +0200

    Minecraft clase TreeDescender

--- src/main/java/es/mariaanasanz/ut7/mods/TreeDescender/TreeDescender.java
+++ src/main/java/es/mariaanasanz/ut7/mods/TreeDescender/TreeDescender.java
@@ -6,6 +6,11 @@ import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.block.state.BlockState;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 public class TreeDescender extends Item {
     public TreeDescender(Properties properties) {
         super(properties);
@@ -15,5 +20,27 @@ public class TreeDescender extends Item {
         // Aquí va la lógica para descender los bloques del árbol.
         return TreeDescender(stack, world, state, pos, entityLiving);
     }
+    public <World, InventoryPlayer> void descenderBloques(BlockPos pos, World world, InventoryPlayer playerInventory) {
+        List<BlockPos> arbolBloques = new
+                ArrayList<BlockPos>();
+        List<BlockPos> bloquesVi = new ArrayList<BlockPos>();
+        Set<BlockPos> visitarBlo = new HashSet<BlockPos>();
 
-}
+        bloquesVi.add(pos);
+        arbolBloques.add(pos);
+
+        while (!bloquesVi.isEmpty()) {
+            BlockPos currentBlock = bloquesVi.remove(0);
+            visitarBlo.add(currentBlock);
+
+            for (BlockPos adjacentBlock : getAdjacentBlocks(currentBlock)) {
+                if (isTreeBlock(adjacentBlock, world)) {
+                    if (!visitarBlo.contains(adjacentBlock)) {
+                        bloquesVi.add(adjacentBlock);
+                        arbolBloques.add(adjacentBlock);
+                    }
+                }
+            }
+        }
+    }}
+
